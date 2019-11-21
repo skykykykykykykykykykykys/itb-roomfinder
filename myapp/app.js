be3bd9a6d61bb13d85b1a7f0e1aff47e83559a18
Ismail F Aziz <25928284+skykykykykykykykykykykys@users.noreply.github.com>
2019-11-21 22:43:03 +0700

    renames

--- myapp/app.js
+++ myapp/app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var dashboardRouter = require('./routes/dashboard');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', dashboardRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
