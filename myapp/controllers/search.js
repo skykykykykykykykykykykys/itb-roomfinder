65e17bcecf87589845e93a3a35f56c30fa26785f
NathiqU <45428114+badasspotatoo@users.noreply.github.com>
2019-11-22 20:58:55 +0700

    connect poo coookk
    juamboooottt

--- myapp/controllers/search.js
+++ myapp/controllers/search.js
@@ -14,9 +14,9 @@
           getDeskripsiGedung,
           getLokasiGedung, return latitude dan longitude
 */
-const db = require('../data/config.js');
+/* const db = require('../data/config.js'); */
 
-exports.getNamaRuang = (payload) => {
+/* exports.getNamaRuang = (payload) => {
      return new Promise((resolve, reject) => {
           const info = [
                payload.idRuang,
@@ -33,4 +33,63 @@ exports.getNamaRuang = (payload) => {
                })
           })
 }
+*/
 
+const {Client} = require ("pg")
+const express = require ("express")
+const app = express();
+
+const client = new Client({
+     "user" : "postgres",
+     "password" : "990830",
+     "host" : "potatooo",
+     "port" : "5432",
+     "database" : "ruangan"
+})
+
+
+start()
+async function start() {
+     await connect();
+     const ruangan = await readRuangan();
+     console.log(ruangan)
+}
+
+async function connect() {
+     try {
+          await client.connect();
+     }
+     catch(e) {
+          console.error('Failed to connect')
+     }
+}
+
+async function readRuangan() {
+     try {
+          const results = await client.query("select idRuangan, namaRuangan from ruangan_type");    
+          return  results.rows;
+     } 
+          catch(e){
+               return[];
+          }
+}
+
+async function createRuangan(ruangantext) {
+     try {
+          await client.query("insert into ruangan_type (text) values ($1)", [ruangantext]);
+          return true;
+     }
+     catch(e){
+          return false;
+     }
+}
+
+async function deleteRuangan(idRuangan){
+     try {
+          await client.query("delete from ruangan_type where idRuangan = $1", [idRuangan]);
+          return true
+     }
+     catch(e){
+          return false;
+     }
+}
